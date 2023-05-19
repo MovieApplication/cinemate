fbf738e8c5a5cf707bdb58b7f20a7019a3fc3887
doyeon <pdy@mtn.co.kr>
2023-05-19 17:44:10 +0900

    로그아웃 임시 추가

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -2,13 +2,28 @@ import {useEffect, useState} from "react"
 import Image from "next/image"
 import Link from "next/link"
 import header from './Header.module.scss'
-import {KAKAO_AUTH_URL} from "services/common"
+import {KAKAO_AUTH_URL, KAKAO_LOGOUT_URL} from "services/common"
 import KakaoLogo from "../../../public/images/kakao_login_small.png"
+import axios from "axios"
 
 export default function Header() {
   const [scrollValue, setScrollValue] = useState(0)
 
   const updateScroll = () => setScrollValue(window.scrollY || document.documentElement.scrollTop)
+
+  // 카카오 로그아웃
+  const fnGetKakaoLogout = async () => {
+    await axios({
+      method: 'GET',
+      url: KAKAO_LOGOUT_URL,
+    }).then(() => {
+      try {
+        alert('로그아웃')
+      } catch (e) {
+        console.log('e : ', e)
+      }
+    })
+  }
 
   useEffect(() => {
     // scroll event
@@ -29,6 +44,10 @@ export default function Header() {
       <Link href={KAKAO_AUTH_URL}>
         <Image src={KakaoLogo} alt="카카오 로그인"/>
       </Link>
+
+      <button type='button' onClick={fnGetKakaoLogout}>
+        로그아웃
+      </button>
     </header>
   )
 }
